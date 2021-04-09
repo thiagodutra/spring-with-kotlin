cc412b49643a422982919f89a9e69a6d9773ad57
tdutra <thiagodutra@gmail.com>
2021-04-08 21:07:37 -0300

    Starting Student feature

--- src/main/kotlin/com/github/thiagodutra/api/controller/StudentController.kt
+++ src/main/kotlin/com/github/thiagodutra/api/controller/StudentController.kt
@@ -1,4 +1,14 @@
 package com.github.thiagodutra.api.controller
 
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@Validated
+@RestController
+@RequestMapping("/student")
 class StudentController {
+
+
+
 }
